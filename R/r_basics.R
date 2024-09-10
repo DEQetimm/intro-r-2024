d2393f75e3d042734cb5d66be3b0d4e0c60199ac
DEQetimm <erica.timm@deq.oregon.gov>
2024-09-10 15:18:26 -0700

    worked on operators

--- R/r_basics.R
+++ R/r_basics.R
@@ -12,8 +12,55 @@ TRUE
 F
 
 
-
 # Operators ----
+#Arithmetic
+2 + 3 # note the spacing, spaces matter for legibility and error checking.
+2 - 3
+2 * 3 # multiplication
+2 / 3
+2 ** 3 #but be careful this is an exponent
+
+
+# Comparison
+2 = 2 #NOT equal, error
+2 == 2 # tests for equality
+
+"Joe" == "joe" # case sensitive
+
+2 == 1 + 1 # Ok
+2 == (1 + 1) # better
+
+2 != 1 #tests inequality
+2 != 2 #tests inequality
+
+2 < 3
+2 > 3
+2 <= 2
+2 >= 3
+
+# somewhat tricky
+TRUE == 1 #
+FALSE == 0
+isTRUE(TRUE) # testing if the argument is literally TRUE
+isTRUE(1)
+?isTRUE # queries built-in help
+
+2 < 3 & 2 > 3 # both have to pass to return TRUE
+2 < 3 | 2 > 3 # either one TRUE, all TRUE; pipe key (above the enter key) is "OR"
+
+2 < 3 & (2 == 1 | 2 == 2) # use parenthesis to group statements for ordering
+
+# type matters (sometimes)
+"joe" # string or character type
+typeof("joe")
+42 # numeric type (double precision, floating point)
+typeof(42)
+TRUE
+typeof(TRUE) # logical or boolean type
+
+42 == "42" # equality can cross types
+identical(42, "42") # type matters for identity
+
 
 # variables ----
 
